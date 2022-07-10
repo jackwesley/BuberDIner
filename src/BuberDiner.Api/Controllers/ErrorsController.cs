afbecadf88f3571d08e242796166cf606c521c86
Jack Wesley Moreira <jackwesley@hotmail.com>
2022-07-10 09:45:19 -0400

    Add project files.

---
+++ src/BuberDiner.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDiner.Api.Controllers
+{
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorsController : Controller
+    {
+        [Route("/error")]
+        public IActionResult Index()
+        {
+            return Problem();
+        }
+    }
+}
